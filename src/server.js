be3275afc70fd9898d2ce13366bbfe54711cee65
Yatin Sarbalia <yatinsns@gmail.com>
2016-03-21 01:35:42 +0530

    Rewrite server to use express

--- src/server.js
+++ src/server.js
@@ -1,27 +1,22 @@
-var http = require('http');
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
 var PORT = 8000;
 var HOSTNAME = 'localhost';
 
-var server = http.createServer(function (request, response) {
-  console.log('New connection received');
-  var setResponseError = function (statusCode, message) {
-    response.writeHead(statusCode, {'Content-Type': 'text/plain'});
-    response.end(message);
-  };
+var app = express();
 
-  if (request.method == 'GET') {
-    if (request.url == '/') {
-      response.statusCode = 200;
-      response.end('Welcome');
-    } else {
-      setResponseError(404, 'Resource not available. Try \'/\' only.');
-    }
-  } else {
-    setResponseError(404, 'Error - Supported HTTP method: GET');
-  }
+var router = express.Router();
+router.use(bodyParser.json());
+router.route('/').post(function (request, response, next) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Got it via post');
 });
 
-server.listen(PORT, HOSTNAME, function () {
+app.use(morgan('dev'));
+app.use('/', router);
+
+app.listen(PORT, HOSTNAME, function () {
   console.log('Server running at ' + HOSTNAME + ':' + PORT + ' ...');
 });
