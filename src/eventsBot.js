28037e695cece213c7be345b9d46bd0bcb9adcae
Yatin Sarbalia <yatinsns@gmail.com>
2016-03-27 17:03:39 +0530

    Handle possible command related to calendar service

--- src/eventsBot.js
+++ src/eventsBot.js
@@ -1,10 +1,14 @@
 var CreateEventsBot = function () {
   this.parseCommand = function (command, callback) {
     console.log('Parsing command : ' + JSON.stringify(command));
-    if (command.match(/^add event/i)) {
+    if (command.match(/^add event$/i)) {
       callback(null, 'Please enter new event details');
     } else if (command.match(/^events$/i)) {
       callback(null, 'Will be listing all events');
+    } else if (command.match(/^edit event$/i)) {
+      callback(null, 'Will be editing event');
+    } else if (command.match(/^delete event$/i)) {
+      callback(null, 'Will be deleting event');
     } else {
       callback(new Error('Unknown message'));
     }
